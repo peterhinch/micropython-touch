89bc4521fe483e56fc4cd842f967ba5d9b3d2015
Peter Hinch <peter@hinch.me.uk>
2024-03-02 11:35:40 +0000

    Add files to index.

---
+++ gui/demos/simple.py
@@ -0,0 +1,41 @@
+# simple.py Minimal touch-gui demo.
+
+# Released under the MIT License (MIT). See LICENSE.
+# Copyright (c) 2021-2024 Peter Hinch
+
+# hardware_setup must be imported before other modules because of RAM use.
+import hardware_setup  # Create a display instance
+from gui.core.tgui import Screen, ssd
+
+from gui.widgets import Label, Button, CloseButton
+from gui.core.writer import CWriter
+
+# Font for CWriter
+import gui.fonts.arial10 as arial10
+from gui.core.colors import *
+
+
+class BaseScreen(Screen):
+    def __init__(self):
+        def my_callback(button, arg):
+            print("Button pressed", arg)
+
+        super().__init__()
+        # verbose default indicates if fast rendering is enabled
+        wri = CWriter(ssd, arial10, GREEN, BLACK)
+        col = 2
+        row = 2
+        Label(wri, row, col, "Simple Demo")
+        row = 50
+        Button(wri, row, col, text="Yes", callback=my_callback, args=("Yes",))
+        col += 60
+        Button(wri, row, col, text="No", callback=my_callback, args=("No",))
+        CloseButton(wri)  # Quit the application
+
+
+def test():
+    print("Simple demo: button presses print to REPL.")
+    Screen.change(BaseScreen)  # A class is passed here, not an instance.
+
+
+test()
---
+++ gui/primitives/__init__.py
@@ -0,0 +1,42 @@
+# __init__.py Common functions for uasyncio primitives used bu ugui
+
+# Copyright (c) 2018-2022 Peter Hinch
+# Released under the MIT License (MIT) - see LICENSE file
+
+try:
+    import uasyncio as asyncio
+except ImportError:
+    import asyncio
+
+
+async def _g():
+    pass
+
+
+type_coro = type(_g())
+
+# If a callback is passed, run it and return.
+# If a coro is passed initiate it and return.
+# coros are passed by name i.e. not using function call syntax.
+def launch(func, tup_args):
+    res = func(*tup_args)
+    if isinstance(res, type_coro):
+        res = asyncio.create_task(res)
+    return res
+
+
+_attrs = {
+    "Delay_ms": "delay_ms",
+}
+
+# Copied from uasyncio.__init__.py
+# Lazy loader, effectively does:
+#   global attr
+#   from .mod import attr
+def __getattr__(attr):
+    mod = _attrs.get(attr, None)
+    if mod is None:
+        raise AttributeError(attr)
+    value = getattr(__import__(mod, None, None, True, 1), attr)
+    globals()[attr] = value
+    return value
